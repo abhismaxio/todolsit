cbb69dd5d8f94f5080878a9d24e9abc924e81bfe
Abhishek <84639530+abhismaxio@users.noreply.github.com>
2023-11-02 02:03:02 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,19 +1,3 @@
-
-// module.exports.getDate= getDate;
-exports.getDate=getDate;
-function getDate(){
-    let today = new Date();
-
-    let options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-    
-    let day =today.toLocaleDateString("en-US",options);  // we can directly return this
-    return day;
-}
-
 exports.getDay=function(){
     let today = new Date();
 
